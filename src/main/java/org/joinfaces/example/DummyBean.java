0070812ec4867c4a81f4145ec6626ded6a8710a9
Lars Grefer <github@larsgrefer.de>
2017-09-17 14:25:08 +0200

    add Example code

---
+++ src/main/java/org/joinfaces/example/DummyBean.java
@@ -0,0 +1,16 @@
+package org.joinfaces.example;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Lars Grefer
+ */
+@Component("dummy")
+public class DummyBean {
+
+    public String getText() {
+        return "Hello from Spring: " + LocalDateTime.now().toString();
+    }
+}
---
+++ src/main/java/org/joinfaces/example/JarExampleApplication.java
@@ -0,0 +1,15 @@
+package org.joinfaces.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Lars Grefer
+ */
+@SpringBootApplication
+public class JarExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarExampleApplication.class, args);
+    }
+}
