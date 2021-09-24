259ed7afdafc1417a727234215f9de5db935b4df
cd2357 <15956136+cd2357@users.noreply.github.com>
2021-09-24 08:32:17 +0200

    Add log hint with test page URL

--- src/main/java/org/joinfaces/example/JarExampleApplication.java
+++ src/main/java/org/joinfaces/example/JarExampleApplication.java
@@ -11,5 +11,12 @@ public class JarExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JarExampleApplication.class, args);
+        
+        // Hint: with the default setup no welcome-page is configured,
+        // so opening the root path "/" results in a 404 error
+        // The URL has to include the page name as well, even for index.xhtml
+        Logger.getLogger(TestPrimefacesApplication.class.toString()).
+                info("Example page available at http://localhost:8080/index.xhtml");
+
     }
 }
